ff67c03f1b7273f615fc61ebda6a5d737ba1e9c0
jckylalaina <jlrandrianarivony@gmail.com>
2026-02-21 10:45:55 +0300

    fix: reset session

--- packages/core-store-session/src/lib/hooks/use-protected-page.tsx
+++ packages/core-store-session/src/lib/hooks/use-protected-page.tsx
@@ -1,19 +1,19 @@
 import { useOAuthStore } from '@maas/core-store-oauth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useSessionStore } from '../store/session-store';
 
 export function useProtectedPage() {
-  const accessToken = useOAuthStore((state) => state.accessToken);
-  const navigate = useNavigate();
-  const location = useLocation();
+    const accessToken = useOAuthStore((state) => state.accessToken);
+    const resetSession = useSessionStore((state) => state.resetSession);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  useEffect(() => {
-    if (!accessToken) {
-      localStorage.setItem(
-        'target-url',
-        `${location.pathname}${location.search}`,
-      );
-      navigate('/login');
-    }
-  }, [accessToken, navigate, location.pathname, location.search]);
+    useEffect(() => {
+        if (!accessToken) {
+            resetSession();
+            localStorage.setItem('target-url', `${location.pathname}${location.search}`);
+            navigate('/login');
+        }
+    }, [accessToken, navigate, location.pathname, location.search, resetSession]);
 }
